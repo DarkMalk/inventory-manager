58435f9c6bf82710ff86766850ba2a7b197a3f6b
darkmalk <sa.vergaramunoz@gmail.com>
2025-02-09 17:58:28 -0300

    Add EditIcon and TrashIcon components

---
+++ web/src/icons/edit-icon.tsx
@@ -0,0 +1,15 @@
+export const EditIcon = () => (
+  <svg
+    width='24'
+    height='24'
+    fill='none'
+    stroke='currentColor'
+    strokeWidth='2'
+    strokeLinecap='round'
+    strokeLinejoin='round'
+    className='icon icon-tabler icons-tabler-outline icon-tabler-pencil'
+  >
+    <path d='M0 0h24v24H0z' stroke='none' />
+    <path d='M4 20h4L18.5 9.5a2.828 2.828 0 1 0-4-4L4 16v4M13.5 6.5l4 4' />
+  </svg>
+)
